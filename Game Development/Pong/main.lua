4255eea4389b1961d4a764f7786f11899c2390d3
Laura Riffo <laurariffo@Lauras-MacBook-Pro.local>
2020-04-15 20:02:18 -0400

    new projects and updates in web development

---
+++ Game Development/Pong/main.lua
@@ -0,0 +1,17 @@
+push = require 'push'
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
+function love.load()
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false,
+        resizeable = false,
+        vsync = true
+    })
+end
